6b8123cac4f26341968d7f2ef9f00ffc00e8cc6b
hollywoodno <hollywoodno@gmail.com; GIT_COMMITTER_NAME=hollywoodno>
2020-02-14 08:54:35 -0800

    layout intro view
    setup quiz introduction layout

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Akyya Mayberry on 2/14/20.
+//  Copyright © 2020 Mayberry, Akyya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
